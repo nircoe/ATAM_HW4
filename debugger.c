3e07c95a180493cbf4c16c3bef703b6fc18b4338
nircoe <nircoe@gmail.com>
2022-06-26 14:04:16 +0000

    part3 and start of part 4

--- debugger.c
+++ debugger.c
@@ -22,7 +22,7 @@ int check_exec(FILE* file, char* exec_name, Elf64_Ehdr* header)
 	return 0;
 }		
 
-int check_func(FILE* file, char* func_name, Elf64_Ehdr* header, Elf64_Shdr* section_header)
+int check_func(FILE* file, char* func_name, Elf64_Ehdr* header, Elf64_Shdr* section_header, Elf64_Sym* symbol_entry)
 {
 	if(fseek(file, header->e_shoff, SEEK_SET) != 0) return -1; // go to section header offset
 	for(int i = 0; i < header->e_shnum; i++) // iterate over section header entries
@@ -33,14 +33,13 @@ int check_func(FILE* file, char* func_name, Elf64_Ehdr* header, Elf64_Shdr* sect
 			if(fseek(file, section_header->sh_offset, SEEK_SET) != 0) return -1; // go to symbol table offset
 			for(int j = 0; j < (section_header->sh_size / section_header->sh_entsize); j++) // iterate over symbol table entries
 			{
-				Elf64_Sym symbol_entry;
-				if(fread(&symbol_entry, sizeof(Elf64_Sym), 1, file) != 1) return -1; // read symbol table entry
-				if(symbol_entry.st_name == func_name) // need to check how to translate func name from Elf64_Word to char*
+				if(fread(symbol_entry, sizeof(Elf64_Sym), 1, file) != 1) return -1; // read symbol table entry
+				if(symbol_entry->st_name == func_name) // need to check how to translate func name from Elf64_Word to char*
 					return 0;
 			}
 		}
 	}
-	
+
 	printf("PRF:: <function name> not found!\n");
 	return -1; // didn't find the function
 	
@@ -66,6 +65,7 @@ int main(int argc, char **argv)
 
 	Elf64_Ehdr* header;
 	Elf64_Shdr* section_header;
+	Elf64_Sym* symbol_entry;
 
 	if(check_exec(file, exec_name, header) != 0) 
 	{	
@@ -73,12 +73,26 @@ int main(int argc, char **argv)
 	}
 	printf("\nFile is Exec!\n");
 
-	if(check_func(file, func_name, header, section_header) != 0)
+	if(check_func(file, func_name, header, section_header, symbol_entry) != 0)
 	{
 		CLOSE_AND_RETURN_ERROR(file);
 	}
 
-	printf("\nFunction exist!\n");
+	if(ELF64_ST_BIND(symbol_entry->st_info) != "GLOBAL") // again, needs to translate string to ...
+	{
+		printf("PRF:: <function name> is not a global symbol! :(\n");
+		CLOSE_AND_RETURN_ERROR(file);
+	}
+	printf("\nFunction exist and it GLOBAL!\n");
+
+	if(symbol_entry->st_shndx == "UND") // function not in file, will get in runtime only (again, translate problem...)
+	{
+
+	}
+	else								// function in file, needs to check which section and search for it
+	{
+
+	}
 
 	fclose(file);
 	return 0;
